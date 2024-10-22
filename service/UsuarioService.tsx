2fee6218aa7c9f140080f964a23e45bd3796a4d8
carloscmp <99369400+carloscmp@users.noreply.github.com>
2024-10-22 14:19:33 -0300

    Criação do CRUD generico na classe "BaseService" aplicando o conceito de herança

--- service/UsuarioService.tsx
+++ service/UsuarioService.tsx
@@ -1,25 +1,8 @@
 import axios from "axios";
+import { BaseService } from "./BaseService";
 
-
-export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080"
-})
-
-export class UsuarioService {
-    listasTodos() {
-        return axiosInstance.get("/usuario")
-    }
-
-
-    inserir(usuario: Projeto.Usuario) {
-        return axiosInstance.post("/usuario", usuario)
-    }
-
-    alterar(usuario: Projeto.Usuario) {
-        return axiosInstance.put("/usuario", usuario);
-    }
-
-    excluir(id: number) {
-        return axiosInstance.delete("/usuario/" + id)
+export class UsuarioService extends BaseService {
+    constructor() {
+        super("/usuario");
     }
 }
