e994fca5365895c9158e46bfcf57e20395d77a97
carloscmp <99369400+carloscmp@users.noreply.github.com>
2025-04-15 09:52:08 -0300

    Retrição de pagina ok tela de login com autenticação ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,60 @@
 'use client';
-import { LayoutProvider } from '../layout/context/layoutcontext';
-import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/primereact.css';
+import { usePathname } from 'next/navigation';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
-import '../styles/layout/layout.scss';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primereact/resources/primereact.css';
+import { useEffect, useState } from 'react';
+import { LayoutProvider } from '../layout/context/layoutcontext';
 import '../styles/demo/Demos.scss';
+import '../styles/layout/layout.scss';
+import LoginPage from './(full-page)/auth/login/page';
+LoginPage;
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
+const checkAuth = () => {
+    if (localStorage.getItem('TOKEN_APLICACAO_FRONTEND') != undefined) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 export default function RootLayout({ children }: RootLayoutProps) {
+    const [pageLoaded, setPageLoaded] = useState(false);
+    const [autenticado, setAutenticado] = useState(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if (pathname.startsWith('/pages') || pathname == '/') {
+            setAutenticado(checkAuth());
+            setPageLoaded(true);
+        } else {
+            setAutenticado(true);
+            setPageLoaded(true);
+        }
+    }, [pathname]);
+
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
                 <link id="theme-css" href={`/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
             </head>
             <body>
-                <PrimeReactProvider>
-                    <LayoutProvider>{children}</LayoutProvider>
-                </PrimeReactProvider>
+                {autenticado ? (
+                    <PrimeReactProvider>
+                        <LayoutProvider>{children}</LayoutProvider>
+                    </PrimeReactProvider>
+                ) : pageLoaded ? (
+                    <PrimeReactProvider>
+                        <LayoutProvider>
+                            <LoginPage />
+                        </LayoutProvider>
+                    </PrimeReactProvider>
+                ) : null}
             </body>
         </html>
     );
